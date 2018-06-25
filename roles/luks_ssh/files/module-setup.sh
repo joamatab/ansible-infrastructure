13e8b33de1b34c5e368459f297d4e0a7870fa6fc
Sheogorath <sheogorath@shivering-isles.com>
2018-06-25 22:17:20 +0200

    Add ssh support to preboot environment to unlock lucks
    This module adds support for SSH to dracut so you can ssh into the 
    machine as soon as its booted and unlock luks.
    
    It uses the machines ed25519 key and you can specify a separate or the 
    same key to auth to your preboot environment.
    
    Inspired by this repository: https://github.com/gsauthof/dracut-sshd

---
+++ roles/luks_ssh/files/module-setup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# 2018, Georg Sauthoff <mail@gms.tf>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# called by dracut
+check() {
+    require_binaries sshd || return 1
+    # 0 enables by default, 255 only on request
+    return 0
+}
+
+# called by dracut
+depends() {
+    # e.g. CentOS 7 doesn't has systemd-networkd
+    if [ -f /usr/lib/systemd/systemd-networkd ]; then
+        # it's more lightweight than using the ifcfg dracut module
+        # and it isn't enabled, by default
+        echo systemd-networkd
+    fi
+    # we don't need to depend on network because ifcfg already depends on it
+    # and ifcfg is enabled, by default, in CentOS 7/Fedora 27/28
+    # echo network
+}
+
+# called by dracut
+install() {
+    local ssh_host_key authorized_keys
+    if [ -f /etc/ssh/dracut_ssh_host_ed25519_key ]; then
+        ssh_host_key=/etc/ssh/dracut_ssh_host_ed25519_key
+    else
+        ssh_host_key=/etc/ssh/ssh_host_ed25519_key
+    fi
+    authorized_keys=${moddir}/authorized_keys
+
+    inst_simple "$ssh_host_key".pub /etc/ssh/ssh_host_ed25519_key.pub
+    /usr/bin/install -m 600 "$ssh_host_key" \
+            "$initdir/etc/ssh/ssh_host_ed25519_key"
+
+    mkdir -p -m 700 "$initdir/root/.ssh"
+    chmod 700 "$initdir/root"
+    /usr/bin/install -m 600 "$authorized_keys" \
+            "$initdir/root/.ssh/authorized_keys"
+
+    inst_multiple /usr/sbin/sshd \
+        /etc/sysconfig/sshd
+    # First entry for Fedora 28, second for Fedora 27
+    inst_multiple -o /etc/crypto-policies/back-ends/opensshserver.config \
+            /etc/crypto-policies/back-ends/openssh-server.config
+    inst_simple "${moddir}/sshd.service" "$systemdsystemunitdir/sshd.service"
+    inst_simple "${moddir}/sshd_config" /etc/ssh/sshd_config
+
+    grep '^sshd:' /etc/passwd >> "$initdir/etc/passwd"
+    grep '^sshd:' /etc/group  >> "$initdir/etc/group"
+
+    mkdir -p "$initdir/var/empty/sshd"
+
+    systemctl --root "$initdir" enable sshd
+
+    # as of Fedora 28, the systemd-networkd dracut module doesn't
+    # include those files
+    inst_multiple -o /etc/systemd/network/*
+
+    return 0
+}
